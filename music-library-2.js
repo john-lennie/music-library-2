aa80d7b7f180907ec5eba25ae081dc90c87e0932
John <johnanthonylennie@gmail.com>
2017-05-31 23:35:16 +0000

    fixed the printPlaylists function

---
+++ music-library-2.js
@@ -0,0 +1,79 @@
+var library = {
+  tracks: { t01: { id: "t01",
+                   name: "Code Monkey",
+                   artist: "Jonathan Coulton",
+                   album: "Thing a Week Three" },
+            t02: { id: "t02",
+                   name: "Model View Controller",
+                   artist: "James Dempsey",
+                   album: "WWDC 2003"},
+            t03: { id: "t03",
+                   name: "Four Thirty-Three",
+                   artist: "John Cage",
+                   album: "Woodstock 1952"}
+          },
+  playlists: { p01: { id: "p01",
+                      name: "Coding Music",
+                      tracks: ["t01", "t02"]
+                    },
+               p02: { id: "p02",
+                      name: "Other Playlist",
+                      tracks: ["t03"]
+                    }
+             },
+  returnPlaylists: function (currentPlaylist) {
+    var playlistId = currentPlaylist.id;
+    //console.log(playlistId);
+    var playlistName = currentPlaylist.name;
+   // console.log(playlistName);
+    var numberOfTracks = currentPlaylist.tracks.length;
+    return (playlistId + ": " + playlistName + " - " + numberOfTracks+ " tracks" );
+  },
+  printPlaylists: function () {
+    var libraryPlaylists =  this.playlists;
+    for (var currentPlaylist in libraryPlaylists) {
+      if(libraryPlaylists.hasOwnProperty(currentPlaylist)) {
+        console.log(this.returnPlaylists(libraryPlaylists[currentPlaylist]));
+      }
+    }
+  },
+  returnTracks: function (currentTrack) {
+    var trackId = currentTrack.id;
+    var trackName = currentTrack.name;
+    var trackArtist = currentTrack.artist;
+    var trackAlbum = currentTrack.album;
+   return (trackId + ": " + trackName + " by " + trackArtist + " (" + trackAlbum +")" );
+  },
+  printTracks: function () {
+    var libraryTracks =  this.tracks;
+    for (var currentTrack in libraryTracks) {
+      if(libraryTracks.hasOwnProperty(currentTrack)) {
+        console.log(returnTracks(libraryTracks[currentTrack]));
+      }
+    }
+  },
+  printPlaylist: function (playlistId) {
+    console.log(returnPlaylists(this.playlists[playlistId]));
+    var tracks = this.playlists[playlistId].tracks;
+    for (i = 0; i < tracks.length; i++) {
+       console.log(returnTracks(this.tracks[tracks[i]]));
+    }
+  },
+  addTrackToPlaylist: function (trackId, playlistId) {
+    this.playlists[playlistId].tracks.push(trackId);
+  },
+  uid: function() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+
+  },
+  addTrack: function (name, artist, album) {
+    var id = "t" + uid();
+    this.tracks[id] = {id: id, name: name, artist: artist, album: album};
+  },
+  addPlaylist: function (name) {
+    var id = "p" + uid();
+    this.playlists[id] = {id: id, name: name, tracks: []};
+  }
+}
+
+library.printPlaylists();
